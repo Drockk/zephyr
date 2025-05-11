e69ba832739f9e98b6446a5ab8104e3f20170c8b
Bartosz Zielonka <batziell@gmail.com>
2025-05-11 17:38:08 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Zephyr
+
+A blazing-fast, modern C++ web framework inspired by Spring Boot.
+
+---
---
+++ zephyr/zephyr.cpp
@@ -0,0 +1,12 @@
+﻿// zephyr.cpp : Defines the entry point for the application.
+//
+
+#include "zephyr.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
