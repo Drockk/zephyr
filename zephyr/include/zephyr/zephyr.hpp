fd7c6a53fb7acf1d1fd4b5e33cd638a2d4d7891b
Bartosz Zielonka <batziell@gmail.com>
2026-01-10 07:43:59 +0000

    Cleanup

--- zephyr/include/zephyr/zephyr.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include <zephyr/core/application.hpp>
-#include <zephyr/core/entrypoint.hpp>
--- zephyr/source/network/endpoint.cpp
+++
@@ -1,9 +0,0 @@
-#include "zephyr/network/endpoint.hpp"
-
-#include "zephyr/network/details/protocolConcept.hpp"
-
-namespace zephyr::network
-{
-template class Endpoint<details::TcpTag>;
-template class Endpoint<details::UdpTag>;
-}  // namespace zephyr::network
