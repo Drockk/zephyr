3e6ef0353b1101cf469f5505e088041509fca563
Bartosz Zielonka <batziell@gmail.com>
2026-02-06 20:40:22 +0000

    Start work on UdpServer

--- examples/udp/example_udp_server.cpp
+++ examples/udp/example_udp_server.cpp
@@ -3,10 +3,9 @@
 #include <span>
 #include <string>
 
-#include <plugins/tcpServer/tcpServer.hpp>
 #include <zephyr/zephyr.hpp>
 
-class TcpServerController
+class UdpServer
 {
 public:
     auto onMessage(std::span<std::byte> t_message) -> std::span<std::byte>
@@ -14,12 +13,11 @@ public:
         std::string text(reinterpret_cast<const char*>(t_message.data()), t_message.size());
         std::cout << text << "\n";
 
-        return t_message;
+        return {};
     }
 };
 
 int main()
 {
-    return zephyr::runApplication(
-        zephyr::Application{"TcpServer", plugins::TcpServer<TcpServerController>{"127.0.0.1", 8080}});
+    return zephyr::runApplication(zephyr::Application{"UdpServer"});
 }
